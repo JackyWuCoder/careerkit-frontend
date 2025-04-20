0a309a6bfbde1124469832f8bbf9ea5e20b6052e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-20 05:38:04 -0700

    implement loading icon for api requests

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
 import { StaticPopulator } from "./classes/utils/staticPopulator.js";
+import { LoadingWrapper } from "./classes/utils/visualUtils.js";
 import { APIHub } from "./classes/apiHub.js";
 
 // Populate statics on page
 StaticPopulator.populateInnerHTML({
+    "site-name": siteName,
     "title": titleLabel,
+    "title-description": titleDescriptionLabel,
     "letter-template": letterLabel,
     "job-description": jobDescriptionLabel,
     "submit": submitLabel,
     "generated-letter": generateLabel
 });
 
+const loader = new LoadingWrapper(
+    document.getElementById("loading-icon"),
+    document.getElementById("generated")
+);
+
 // Handle click to submit
 document.getElementById("submit").addEventListener("click", async (event) => {
     event.preventDefault();
+    loader.show();
 
     const template = document.getElementById("template").value;
     const description = document.getElementById("description").value;
@@ -21,5 +30,6 @@ document.getElementById("submit").addEventListener("click", async (event) => {
         template: template,
         description: description
     }));
-    document.getElementById("generated").innerHTML = generatedLetter ? generatedLetter : serverError;
+    document.getElementById("generated").value = generatedLetter ? generatedLetter : serverError;
+    loader.hide();
 })
