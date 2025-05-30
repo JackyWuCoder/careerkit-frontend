0e412563d28149d0a0b08eb3964bff09b106c089
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 02:12:51 -0700

    feat: create components for resume, interview, advice, ai, apitracker

---
+++ client/src/app/advice/advice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advice',
+  standalone: true,
+  imports: [],
+  templateUrl: './advice.component.html',
+  styleUrl: './advice.component.css'
+})
+export class AdviceComponent {
+
+}
---
+++ client/src/app/api-tracker/api-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-tracker.component.html',
+  styleUrl: './api-tracker.component.css'
+})
+export class ApiTrackerComponent {
+
+}
