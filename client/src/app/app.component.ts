2d7ef94734531dcc085df8a676816312726d2a77
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-29 23:48:09 -0700

    feat: created a basic navbar component with some headers and styling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component'
 import { CoverLetterComponent } from './cover-letter/cover-letter.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoverLetterComponent],
+  imports: [RouterOutlet, NavComponent, CoverLetterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
