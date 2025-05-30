fb6e1d232a3c9ddf5202a2110bf700bda1a7a8d4
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 01:52:34 -0700

    feat: implement a cover letter service that calls the /coverLetter/generate/template endpoint

--- client/src/app/services/cover-letter.service.ts
+++ client/src/app/services/cover-letter.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoverLetterService {
+  private apiUrl = "http://localhost:5240/api/v1/coverLetter/generate/template";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  generateCoverLetter(body: any): Observable<string | null> {
+    return this.http.post<{ result: string }>(this.apiUrl, body, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json'}),
+    }).pipe(
+      map(response => response.result),
+      catchError((err) => {
+        console.error('Error from API:', err);
+        return of(null);
+      }),
+    )
+  }
 }
