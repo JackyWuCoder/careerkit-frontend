73ffd2fa32385ac5d060f78a47c0801447e4e3d2
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 01:55:54 -0700

    feat: cover letter service will be called by a generate function inside the cover letter component
    - the generate function is triggered on a button press and passes in a "Template" and "JobDescription", which matches the label of the backend "FillCoverLetterRequest" DTO

--- client/src/app/cover-letter/cover-letter.component.ts
+++ client/src/app/cover-letter/cover-letter.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CoverLetterService } from '../services/cover-letter.service'
 
 @Component({
   selector: 'app-cover-letter',
@@ -13,10 +14,26 @@ export class CoverLetterComponent {
   coverLetter = '';
   jobDescription = '';
   generatedLetter = '';
+  isLoading = false;
+
+  constructor(private coverLetterService: CoverLetterService) {}
 
   generate() {
-    this.generatedLetter = `AI response based on:
-    - Your letter: ${this.coverLetter}
-    - Job: ${this.jobDescription}`;
+    this.isLoading = true;
+
+    this.coverLetterService.generateCoverLetter({
+      Template: this.coverLetter,
+      JobDescription: this.jobDescription
+    }).subscribe({
+      next: (result) => {
+        this.generatedLetter = result || 'Failed to generate letter.';
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.generatedLetter = 'An error occurred while generating the letter.';
+        this.isLoading = false;
+      }
+    });
   }
 }
