5a56ece41a92fccd02a2791dc28e489441395a7e
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 02:09:23 -0700

    feat: set up main routes for CareerKit pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { CoverLetterComponent } from './cover-letter/cover-letter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'cover-letter', component: CoverLetterComponent},
+    { path: 'resume', component: HomeComponent },
+    { path: 'interview', component: HomeComponent },
+    { path: 'advice', component: HomeComponent },
+    { path: 'ai', component: HomeComponent },
+    { path: 'tracker', component: HomeComponent },
+];
