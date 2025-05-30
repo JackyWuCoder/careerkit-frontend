9bf52d2b7261e21f6fe62e8358ec83e90729ab2f
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 02:13:55 -0700

    feat: added the correct routing components for each path name representing a service

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { CoverLetterComponent } from './cover-letter/cover-letter.component';
+import { ResumeComponent } from './resume/resume.component';
+import { InterviewComponent } from './interview/interview.component';
+import { AdviceComponent } from './advice/advice.component';
+import { AiComponent } from './ai/ai.component';
+import { ApiTrackerComponent } from './api-tracker/api-tracker.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'cover-letter', component: CoverLetterComponent},
-    { path: 'resume', component: HomeComponent },
-    { path: 'interview', component: HomeComponent },
-    { path: 'advice', component: HomeComponent },
-    { path: 'ai', component: HomeComponent },
-    { path: 'tracker', component: HomeComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'interview', component: InterviewComponent },
+    { path: 'advice', component: AdviceComponent },
+    { path: 'ai', component: AiComponent },
+    { path: 'tracker', component: ApiTrackerComponent },
 ];
