e7f5558ff8f05956520cb29030ceb6a6927208cf
Jacky Wu <JackyWuCoder@gmail.com>
2025-05-30 00:09:13 -0700

    chore: imported FormsModule and CommonModule
    1) FormsModule allows the use of something like [(ngModel)]="userLetter" such that the component template can use created variables inside the component logic
    
    2) CommonModule allows the use of *ngIf, where it is able to show the element based on its conditional value of being '' (empty) or not

--- client/src/app/cover-letter/cover-letter.component.ts
+++ client/src/app/cover-letter/cover-letter.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cover-letter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cover-letter.component.html',
   styleUrl: './cover-letter.component.css'
 })
 export class CoverLetterComponent {
+  userLetter = '';
+  jobDescription = '';
+  generatedLetter = '';
 
+  generate() {
+    this.generatedLetter = `AI response based on:
+    - Your letter: ${this.userLetter}
+    - Job: ${this.jobDescription}`;
+  }
 }
